5c3d38fd6932e92fe0f643d6fcdff9e2a9390de3
s24egao <99890186+s24egao@users.noreply.github.com>
2022-07-19 21:58:57 +0800

    change swipe direction

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ for(let item of gallery) {
 	let link = (item.link)? `<a href="${item.link}" target="_blank">Open Link</a>` : ``
 	if(item.src.endsWith('.jpg')) media = `<img src="${item.src}" alt="" draggable="false"}">`
 	if(item.src.endsWith('.mp4')) media = `<video autoplay loop muted playsinline disablepictureinpicture><source src="${item.src}"></video>`
-	$(`#contents`).append(`<div class="image" style="animation: slide-up 0.5s ${i * 0.1}s backwards; flex: ${item.width} 1 ${item.width / 3}px;">${media}${link}</div>`)
+	$(`#contents`).append(`<div id="image_${i}" class="image" style="animation: slide-up 0.5s ${i * 0.1 + 0.2}s backwards;">${media}${link}</div>`)
+	
+	let mediaElement = document.getElementById(`image_${i}`) 
+	if(item.src.endsWith('.jpg')) mediaElement.childNodes[0].onload = () => { 
+		let aspect = mediaElement.childNodes[0].naturalWidth / mediaElement.childNodes[0].naturalHeight
+		mediaElement.setAttribute('style', `${mediaElement.getAttribute('style')} flex: ${aspect} 1 ${aspect * 175}px`)
+	}
+	if(item.src.endsWith('.mp4')) mediaElement.childNodes[0].onloadeddata = () => { 
+		let aspect = mediaElement.childNodes[0].videoWidth / mediaElement.childNodes[0].videoHeight
+		mediaElement.setAttribute('style', `${mediaElement.getAttribute('style')} flex: ${aspect} 1 ${aspect * 175}px`)
+	}
 }
+$(`#contents`).append(`<div class="image" style="flex: 1 1 300px; margin-top: 0px; margin-bottom: 0px;"></div>`)
 
 let info_page = 0
 
@@ -23,31 +34,31 @@ let mouse_x
 function touch_start(e) {
 	if(window.innerWidth > 720) return
 	swipe = true
-	let position_x = (e.type.startsWith('touch'))? e.touches[0].clientX : e.clientX
+	let position_x = (e.type.startsWith('touch'))? e.touches[0].clientY : e.clientY
 	$('#info').css('transition-duration', '0s')
 	$('#arrow-up').css('transform', 'rotate(90deg)')
 	$('#arrow-down').css('transform', 'rotate(-90deg)')
 	$('#arrow').css('animation', 'none')
-	mouse_x = position_x - parseFloat($('#info').css('left'))
+	mouse_x = position_x - parseFloat($('#info').css('top'))
 }
 
 function touch_move(e) {
 	if(!swipe || window.innerWidth > 720) return
-	let position_x = (e.type.startsWith('touch'))? e.touches[0].clientX : e.clientX
+	let position_x = (e.type.startsWith('touch'))? e.touches[0].clientY : e.clientY
 	if(position_x - mouse_x >= 0) return
-	$('#info').css('left', `${position_x - mouse_x}px`)
+	$('#info').css('top', `${position_x - mouse_x}px`)
 }
 
 function touch_end(e) {
 	if(!swipe || window.innerWidth > 720) return
 	swipe = false
-	let position_x = (e.type.startsWith('touch'))? e.changedTouches[0].clientX : e.clientX
+	let position_x = (e.type.startsWith('touch'))? e.changedTouches[0].clientY : e.clientY
 	$('#info').css('transition-duration', '0.5s')
 	$('#arrow-up').css('transform', 'rotate(60deg)')
 	$('#arrow-down').css('transform', 'rotate(-60deg)')
 	$('#arrow').css('animation', 'arrow 1s 0s infinite alternate-reverse')
-	if((!show_info && position_x - mouse_x < -30)) $('#info').css('left', '-92%').css('opacity', '0.2')
-	else $('#info').css('left', '0px').css('opacity', '1')
+	if((!show_info && position_x - mouse_x < -30)) $('#info').css('top', '-93%').css('opacity', '0.2')
+	else $('#info').css('top', '0px').css('opacity', '1')
 	show_info = !show_info
 }
 
@@ -60,5 +71,5 @@ $(window).resize(() => {
 	if(window.innerWidth <= 720) return
 	swipe = false
 	show_info = true
-	$('#info').css('transition-duration', '0.5s').css('left', '0px').css('opacity', '1')
+	$('#info').css('transition-duration', '0.5s').css('top', '0px').css('opacity', '1')
 })
